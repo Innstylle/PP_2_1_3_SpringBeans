7bdf3d66a9ea4c4df50cde7517d1e7dbe0548a58
Kirill <vkashe1337@gmail.com>
2024-02-08 14:18:55 +0300

    Comp-t!

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -2,7 +2,6 @@ package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,16 +10,17 @@ public class AnimalsCage {
     @Autowired
     @Qualifier("dog")
     private Animal animal;
-    private final Timer timer = new Timer();
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(timer.getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
-    @Bean
+
     public Timer getTimer() {
         return timer;
     }
